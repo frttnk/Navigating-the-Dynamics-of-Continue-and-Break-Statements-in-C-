de3512291867673ee7ac10234660e7c0d4ee1c97
frttnk <firattonak@gmail.com>
2023-01-20 22:25:21 -0500

    Continue - Break statements

---
+++ ContinueBreakStatements/ContinueBreakStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    if (i == 7)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(i);
+//}
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 7)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
